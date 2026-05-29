6a931df7e590ec01e003ff4b6ffe9b3300bcefbe
Noah Brinkman <noahpbrinkman@gmail.com>
2026-05-29 21:11:10 +0200

    DoTween and start of minimap

---
+++ Assets/Scripts/Minimap/MinimapHandler.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MinimapHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private RectTransform minimapParent;
+    [SerializeField] private float minimapResolution;
+    [SerializeField] private Transform bottomLeft;
+    [SerializeField] private Transform TopRight;
+    [SerializeField] private Transform bottomRight; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
